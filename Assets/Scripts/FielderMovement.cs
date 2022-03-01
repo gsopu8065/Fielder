a9520132a79b820f82264ed715d7fd52972a21ce
Tomasz Kirsz <tom@cherryengine.co.uk>
2022-03-01 16:55:00 +0100

    Catch the ball update
    1
    If ball is flying higher than his height, apply jump animation
    
    
    2
    If ball is in the plane, player should 2.a: Rotating at the ball, 2.b: Run(Animation) , 3.c: Randomly pick one animation from [Pickup(FielderPickUp), Dive(DiveFront_01_inplace), Roll(Di ... more
    
    3
    Player should not leave the plane

--- Assets/Scripts/FielderMovement.cs
+++ Assets/Scripts/FielderMovement.cs
@@ -1,16 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FielderMovement : MonoBehaviour
 {
-
     CharacterController controller;
 
-    [SerializeField] float speed;
-    [SerializeField] GameObject ball;
-    [SerializeField] GameObject playerBody;
+    [SerializeField] private float rotationSpeed = 3;
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private Transform ballTransform;
+    [SerializeField] private Transform ballCheckTransform;
 
+
+    [Header("Animations")] 
+    [SerializeField] private Transform highTester;
+    [SerializeField] private Transform jumpHighTester;
+    [SerializeField] private Transform lowTester;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject ballAfterCollect;
+    [SerializeField] private Transform catchTransform;
 
 
     private Vector3 velocity;
@@ -18,76 +26,137 @@ public class FielderMovement : MonoBehaviour
     private float groundCheckDistance = 0.2f;
     [SerializeField] private LayerMask groundMask;
     private float gravity = -9.8f;
+    private Transform _transform;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
+    {
+        _transform = transform; //cache transform for better performance!
+    }
+
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
-        //StartCoroutine(DoRotationAtTargetDirection(playerBody.transform));
+        ballAfterCollect.SetActive(false);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Ball")
+            return;
+        ballAfterCollect.transform.position = ballTransform.position;
+        ballAfterCollect.SetActive(true);
+        Destroy(ballTransform.gameObject);
+        SelectProperAnimation();
+       
+        
+    }
+
+    //we have 3 normal animations
+    //animation from above and animation picking from the ground
+    private void SelectProperAnimation()
+    {
+        float ballTime = .15f;
+
+        if (ballTransform.position.y > jumpHighTester.position.y)
+        {
+            animator.SetTrigger("highJump");
+            ballTime = .25f;
+        }
+        else
+        {
+            if (ballTransform.position.y > highTester.position.y)
+            {
+                animator.SetTrigger("jump");
+                ballTime = .2f;
+            }
+            else if (ballTransform.position.y < lowTester.position.y)
+            {
+                animator.SetTrigger("pickup");
+                ballTime = .2f;
+            }
+            else
+            {
+                animator.SetInteger("collect", Random.Range(1, 4));
+                Invoke(nameof(ResetCollector), .5f);
+            }    
+        }
+        Invoke(nameof(MoveBallToCatch),ballTime);
+    }
+
+    private void MoveBallToCatch()
+    {
+        ballAfterCollect.transform.SetParent(catchTransform);
+        ballAfterCollect.transform.localPosition = Vector3.zero;
+    }
+
+    private void ResetCollector()
+    {
+        animator.SetInteger("collect", 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (ballTransform == null)
+            return;
+        isGrounded = CheckIfBallIsOnField();
+    }
+
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (!ball) return;
-
-        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
+        if (ballTransform != null)
         {
-            velocity.y = -2f;
+            if (isGrounded)
+            {
+                MoveToBall();
+                RotateToBall();
+            }
         }
 
-        if (isGrounded)
-        {
-            Vector3 ballPosition = ball.transform.position - transform.position;
-            controller.Move(ballPosition * speed * Time.deltaTime);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
-
-        transform.LookAt(ball.transform);
-
-        // The step size is equal to speed times frame time.
-        //var step = speed * Time.deltaTime;
-
-        // Rotate our transform a step closer to the target's.
-        //playerBody.transform.rotation = Quaternion.RotateTowards(playerBody.transform.rotation, ball.transform.rotation, step);
-
-        /* if (ballPosition != Vector3.zero)
-         {
-             // Determine which direction to rotate towards
-             Vector3 targetDirection = ballPosition - playerBody.transform.position;
-
-             // The step size is equal to speed times frame time.
-             float singleStep = 1.0f * Time.deltaTime;
-
-             // Rotate the forward vector towards the target direction by one step
-             Vector3 newDirection = Vector3.RotateTowards(playerBody.transform.forward, targetDirection, singleStep, 0.0f);
-
-             // Draw a ray pointing at our target in
-             Debug.DrawRay(playerBody.transform.position, newDirection, Color.red);
-
-             // Calculate a rotation a step closer to the target and applies rotation to this object
-             playerBody.transform.rotation = Quaternion.LookRotation(newDirection);
-         }*/
+        SetAnimation();
     }
 
-    IEnumerator DoRotationAtTargetDirection(Transform opponentPlayer)
+    private void SetAnimation()
     {
-        Quaternion targetRotation = Quaternion.identity;
-        do
+        var clamp = Math.Clamp(controller.velocity.magnitude, 0f, 1f);
+        animator.SetFloat("speed", clamp);
+    }
+
+    private bool IsControllerInMove()
+    {
+        return controller.velocity.magnitude > .5f;
+    }
+
+    private bool CheckIfBallIsOnField()
+    {
+        ballCheckTransform.position = ballTransform.position; //we need to eliminate rotation
+        if (Physics.Raycast(ballCheckTransform.position, ballCheckTransform.TransformDirection(-Vector3.up),
+                out var hit, Mathf.Infinity, groundMask))
         {
-            Debug.Log("do rotation");
+            Debug.DrawRay(ballCheckTransform.position,
+                ballCheckTransform.TransformDirection(-Vector3.up) * hit.distance, Color.green);
+            return true;
+        }
 
-            Vector3 targetDirection = (opponentPlayer.position - transform.position).normalized;
-            targetRotation = Quaternion.LookRotation(targetDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime);
+        Debug.DrawRay(ballCheckTransform.position, ballCheckTransform.TransformDirection(-Vector3.up) * 1000,
+            Color.red);
+        return false;
+    }
 
-            yield return null;
+    private void MoveToBall()
+    {
+        Vector3 ballPosNoY = new Vector3(ballTransform.position.x, _transform.position.y, ballTransform.position.z);
+        Vector3 ballPosition = ballPosNoY - _transform.position;
+        controller.Move(ballPosition * movementSpeed * Time.deltaTime);
+    }
 
-        } while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f);
+    private void RotateToBall()
+    {
+        Vector3 relativePos = ballTransform.position - _transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        _transform.rotation = Quaternion.Slerp(_transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        _transform.localEulerAngles = new Vector3(0, _transform.localEulerAngles.y, 0);
     }
 }
